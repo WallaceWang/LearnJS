47fa34a14bb0dc1ef9617023fab157e0e0b4e41b
wangxiaorui19 <wangxiaorui19@jd.com>
2021-08-10 10:06:32 +0800

    demo

--- GuessNumber/js/index.js
+++ GuessNumber/js/index.js
@@ -41,3 +41,64 @@ function check() {
         document.body.appendChild(errorP);
     }
 }
+
+const clickBtn = document.querySelector('.clickme');
+// clickBtn.onclick = function(){
+//     let name = prompt('请输入你的名字：');
+//     alert('Hi,' + name + ', nice to meet you!');
+// };
+
+clickBtn.addEventListener('click', function(){
+    let name = prompt('请输入你的名字：');
+    alert('Hi,' + name + ', nice to meet you!');
+});
+
+/*
+// 构造函数
+let Person = function(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayHello = sayHello;
+}
+
+function sayHello() {
+    console.log('hello, ' + this.name);
+}
+
+let p1 = new Person('wallace', 30, '程序员');
+let p2 = new Person('dou', 27, '护士');
+
+p1.sayHello();
+p2.sayHello();
+*/
+
+// 原型模式
+// function Person(){}
+let Person = function() {}
+
+Person.prototype.name = 'wallace';
+Person.prototype.age = 30;
+Person.prototype.sayHello = function(){
+    console.log('hello, ' + this.name);
+}
+
+let p1 = new Person();
+let p2 = new Person();
+
+p1.sayHello();
+p2.sayHello();
+console.log(p1.sayHello === p2.sayHello);
+console.log(Person);
+console.log(Person['prototype']);
+console.log(Person.prototype);
+console.log(Person.prototype.constructor);
+console.log(Person.prototype['constructor']);
+console.log(Person.prototype.constructor == Person);
+console.log(p1.prototype);
+console.log(p1.__proto__);
+console.log(p1.__proto__ == Person.prototype);
+console.log(Person.prototype.__proto__);
+console.log(Person.prototype.__proto__ == Object.prototype);
+console.log(Person.prototype.__proto__.constructor == Object);
+console.log(p2.__proto__.__proto__.constructor == Object);
